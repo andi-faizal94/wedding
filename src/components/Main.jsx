c6358b954eaeb448f069f0dd9e80133adef49a21
Andi Faizal <andifaizal647@gmail.com>
2023-03-03 02:08:24 +0700

    resolve deploy in image

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,7 +4,7 @@ import moby from '../assets/Marry Me.mp3';
 import image1 from '../assets/image1.jpg';
 import image2 from '../assets/image2.jpg';
 import image3 from '../assets/image3.jpg';
-import image4 from '../assets/image4.jpg';
+import image from '../assets/image';
 
 const Sound = new Audio(moby);
 
@@ -155,12 +155,12 @@ const Main = () => {
           />
           <img
             className='w-2/12 h-1/2  md:w-2/12 object-cover rounded-sm'
-            src={image4}
+            src={image}
             alt='images'
           />
           <img
             className='w-2/12 h-1/2  md:w-2/12 object-cover rounded-sm'
-            src={image4}
+            src={image}
             alt='images'
           />
         </div>
